c799be5f84cbd7d9cc1ae41ab2994312436a2d72
Abel <dougbel.unam@gmail.com>
2020-10-02 17:19:02 +0100

    Test it in environments
    ee

---
+++ 11_mpi_new_routines/03_test_on_env/test_it_on_env.py
@@ -0,0 +1,39 @@
+import argparse
+
+from mpi4py import MPI
+
+from mpi_routines.master import MasterTestInteractionEnv
+from mpi_routines.slaves.slave_enviro_tester import SlaveEnviroTester
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset_scans_path', required=True, help='Path to ScanNet dataset')
+parser.add_argument('--work_directory', required=True, help='Path to work_directory folder')
+parser.add_argument('--json_conf_execution_file', required=True,  help='JSON file with execution parameters')
+parser.add_argument('--descriptors_repository', required=True, help='Interactions descriptors path to test in environment')
+opt = parser.parse_args()
+print(opt)
+
+if __name__ == "__main__":
+
+    name = MPI.Get_processor_name()
+    rank = MPI.COMM_WORLD.Get_rank()
+    size = MPI.COMM_WORLD.Get_size()
+
+    print('***************************************************************************')
+    print('I am  %s rank %d (total %d)' % (name, rank, size))
+    print('***************************************************************************')
+
+    if rank == 0:  # Master
+        app = MasterTestInteractionEnv(slaves=range(1, size),
+                                       work_directory=opt.work_directory,
+                                       prefix_follow_up_column="env_test",
+                                       json_conf_execution_file=opt.json_conf_execution_file)
+        app.run()
+        app.terminate_slaves()
+
+    else:  # Any slave
+
+        SlaveEnviroTester(dataset_scans_path=opt.dataset_scans_path,
+                          work_directory=opt.work_directory,
+                          descriptors_repository=opt.descriptors_repository,
+                          json_conf_execution_file=opt.json_conf_execution_file).run()
---
+++ 11_mpi_new_routines/03_test_on_env/test_it_on_env_test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --job-name=envtestt
+#SBATCH --nodes=12
+#SBATCH --ntasks-per-node=3
+#SBATCH --cpus-per-task=8
+#SBATCH --time=05:00:00
+#SBATCH --mem=96G
+#SBATCH --mail-type=ALL
+
+module load OpenMPI/2.0.1-gcccuda-2016.10
+module load languages/anaconda3/3.7
+
+PREFIX_ENV=/mnt/storage/scratch/csapo/conda_envs/trimesh_vedo
+WORKING_DIRECTORY=/mnt/storage/scratch/csapo/analisys/mpi_routines/test
+SCANS_DIR=/mnt/storage/scratch/csapo/ScanNet/scans_test
+TRAINED_ITER_DIR=/mnt/storage/home/csapo/scratch/analisys/mpi_routines/configs/descriptor_repository
+JSON_EXEC_FILE=/mnt/storage/home/csapo/scratch/analisys/mpi_routines/configs/json_execution/single_testing_sitting.json
+
+
+source activate $PREFIX_ENV
+export PATH=$PREFIX_ENV/bin:$PATH
+export PYTHONPATH=/mnt/storage/home/csapo/git_repositories/iTpy:/mnt/storage/home/csapo/git_repositories/iTpyClearance:/mnt/storage/home/csapo/git_repositories/mpi-master-slave:/mnt/storage/home/csapo/git_repositories/mpi_routines:\$PYTHONPATH
+
+srun --mpi=pmi2 python test_it_on_env.py --work_directory $WORKING_DIRECTORY --dataset_scans_path  $SCANS_DIR --descriptors_repository $TRAINED_ITER_DIR --json_conf_execution_file $JSON_EXEC_FILE
