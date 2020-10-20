556b1c53739065ba3e7c840cb6c3e294fbcd5657
Abel Pacheco-Ortega <dougbel.unam@gmail.com>
2020-10-20 12:50:52 +0100

    mpi propagate on rgb

---
+++ 11_mpi_new_routines/05_propagate_on_rgb/propagate_on_rgb.py
@@ -0,0 +1,42 @@
+import argparse
+
+from mpi4py import MPI
+
+from mpi_routines.master import MasterMultiRoutines
+from mpi_routines.slaves.slave_propagate_on_images import SlavePropagateOnRGBImages
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset_scans_path', required=True, help='Path to ScanNet dataset')
+parser.add_argument('--work_directory', required=True, help='Path to work_directory folder')
+parser.add_argument('--json_conf_execution_file', required=True, help='JSON file with execution parameters')
+parser.add_argument('--propagators_directory', required=True, help='Directory with propagators calculated')
+
+opt = parser.parse_args()
+print(opt)
+
+if __name__ == "__main__":
+
+    name = MPI.Get_processor_name()
+    rank = MPI.COMM_WORLD.Get_rank()
+    size = MPI.COMM_WORLD.Get_size()
+
+    print('***************************************************************************')
+    print('I am  %s rank %d (total %d)' % (name, rank, size))
+    print('***************************************************************************')
+
+    if rank == 0:  # Master
+        app = MasterMultiRoutines(slaves=range(1, size),
+                                  work_directory=opt.work_directory,
+                                  prefix_follow_up_column="rgb_prop",
+                                  json_conf_execution_file=opt.json_conf_execution_file)
+        app.run()
+        app.terminate_slaves()
+
+    else:  # Any slave
+
+        SlavePropagateOnRGBImages(dataset_scans_path=opt.dataset_scans_path,
+                                  work_directory=opt.work_directory,
+                                  propagators_directory=opt.propagators_directory,
+                                  json_conf_execution_file=opt.json_conf_execution_file).run()
+
+    print('Task completed (rank %d)' % (rank))
---
+++ 11_mpi_new_routines/05_propagate_on_rgb/propagate_on_rgb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --job-name=proprgb
+#SBATCH --nodes=12
+#SBATCH --ntasks-per-node=3
+#SBATCH --cpus-per-task=8
+#SBATCH --time=15:00:00
+#SBATCH --mem=96G
+#SBATCH --mail-type=ALL
+
+module load OpenMPI/2.0.1-gcccuda-2016.10
+module load languages/anaconda3/3.7
+
+PREFIX_ENV=/mnt/storage/scratch/csapo/conda_envs/trimesh_vedo
+source activate $PREFIX_ENV
+export PATH=$PREFIX_ENV/bin:$PATH
+export PYTHONPATH=/mnt/storage/home/csapo/git_repositories/iTpy:/mnt/storage/home/csapo/git_repositories/iTpyClearance:/mnt/storage/home/csapo/git_repositories/mpi-master-slave:/mnt/storage/home/csapo/git_repositories/mpi_routines:/mnt/storage/home/csapo/git_repositories/si:\$PYTHONPATH
+
+
+SCANS_DIR=/mnt/storage/scratch/csapo/ScanNet/scans
+WORKING_DIRECTORY=/mnt/storage/scratch/csapo/analisys/mpi_routines/train
+JSON_EXEC_FILE=/mnt/storage/home/csapo/scratch/analisys/mpi_routines/configs/json_execution/single_testing_sitting.json
+PROPAGATORS_DIR=/mnt/storage/home/csapo/scratch/analisys/mpi_routines/train/propagators
+
+srun --mpi=pmi2 python propagate_on_rgb.py --dataset_scans_path  $SCANS_DIR --work_directory $WORKING_DIRECTORY --json_conf_execution_file $JSON_EXEC_FILE  --propagators_directory $PROPAGATORS_DIR
