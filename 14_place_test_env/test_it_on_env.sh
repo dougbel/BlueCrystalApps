5db1a264c34a762cdea8c35e0dd12425cef61bbb
Abel Pacheco Ortega <dougbel.unam@gmail.com>
2021-06-09 11:55:13 +0100

    Added: iTClea on PROX, MP3D (partially) and Replica (partially) dataset

---
+++ 14_place_test_env/test_it_on_env.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --job-name=envtest
+#SBATCH --partition veryshort
+#SBATCH --nodes=12
+#SBATCH --ntasks-per-node=3
+#SBATCH --cpus-per-task=8
+#SBATCH --time=6:00:00
+#SBATCH --mem=96G
+#SBATCH --mail-type=ALL
+
+module load OpenMPI/2.0.1-gcccuda-2016.10
+module load languages/anaconda3/3.7
+
+PREFIX_ENV=/mnt/storage/scratch/csapo/conda_envs/trimesh_vedo
+WORKING_DIRECTORY=/mnt/storage/home/csapo/scratch/PLACE_comparisson/test
+SCANS_DIR=/mnt/storage/home/csapo/scratch/PLACE_comparisson/datasets
+CONFIG_DIR=/mnt/storage/home/csapo/scratch/PLACE_comparisson/config
+
+
+source activate $PREFIX_ENV
+export PATH=$PREFIX_ENV/bin:$PATH
+export PYTHONPATH=/mnt/storage/home/csapo/git_repositories/iTpy:/mnt/storage/home/csapo/git_repositories/iTpyClearance:/mnt/storage/home/csapo/git_repositories/mpi-master-slave:/mnt/storage/home/csapo/git_repositories/mpi_routines:.:\$PYTHONPATH
+
+srun --mpi=pmi2 python 01_testing.py --dataset_scans_path $SCANS_DIR --work_directory $WORKING_DIRECTORY  --config_directory $CONFIG_DIR
