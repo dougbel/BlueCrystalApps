a2ef84b00d1e3a1b76e6cedfc375f95ebcdda5e6
Abel Pacheco Ortega <dougbel.unam@gmail.com>
2022-08-09 11:09:00 +0100

    Fix: script to execute routine

--- 17_lander_prox_sdf_generation/PROX_sdf_384_generation.sh
+++ 17_lander_prox_sdf_generation/PROX_sdf_384_generation.sh
@@ -29,26 +29,26 @@ PROX_SCANS_DIR=/user/home/csapo/work/Lander/dataset/prox/scene_artefact_removed
 PROX_OUTPUT_DIR=/user/home/csapo/work/Lander/dataset/prox/scene_artefact_removed/sdf_384
 
 echo "working on BasementSittingBooth"
-srun --mpi=pmi2 python 02_sdf_calculation_following_prox_MPI.py --scans_dir $PROX_SCANS_DIR  --scene BasementSittingBooth --grid_dim $GRID_DIM --output_dir $PROX_OUTPUT_DIR
+srun --mpi=pmi2 python PROX_sdf_calculation_following_prox_MPI.py --scans_dir $PROX_SCANS_DIR  --scene BasementSittingBooth --grid_dim $GRID_DIM --output_dir $PROX_OUTPUT_DIR
 echo "working on MPH11"
-srun --mpi=pmi2 python 02_sdf_calculation_following_prox_MPI.py --scans_dir $PROX_SCANS_DIR  --scene MPH11 --grid_dim $GRID_DIM --output_dir $PROX_OUTPUT_DIR
+srun --mpi=pmi2 python PROX_sdf_calculation_following_prox_MPI.py --scans_dir $PROX_SCANS_DIR  --scene MPH11 --grid_dim $GRID_DIM --output_dir $PROX_OUTPUT_DIR
 echo "working on MPH112"
-srun --mpi=pmi2 python 02_sdf_calculation_following_prox_MPI.py --scans_dir $PROX_SCANS_DIR  --scene MPH112 --grid_dim $GRID_DIM --output_dir $PROX_OUTPUT_DIR
+srun --mpi=pmi2 python PROX_sdf_calculation_following_prox_MPI.py --scans_dir $PROX_SCANS_DIR  --scene MPH112 --grid_dim $GRID_DIM --output_dir $PROX_OUTPUT_DIR
 echo "working on MPH16"
-srun --mpi=pmi2 python 02_sdf_calculation_following_prox_MPI.py --scans_dir $PROX_SCANS_DIR  --scene MPH16 --grid_dim $GRID_DIM --output_dir $PROX_OUTPUT_DIR
+srun --mpi=pmi2 python PROX_sdf_calculation_following_prox_MPI.py --scans_dir $PROX_SCANS_DIR  --scene MPH16 --grid_dim $GRID_DIM --output_dir $PROX_OUTPUT_DIR
 echo "working on MPH1Library"
-srun --mpi=pmi2 python 02_sdf_calculation_following_prox_MPI.py --scans_dir $PROX_SCANS_DIR  --scene MPH1Library --grid_dim $GRID_DIM --output_dir $PROX_OUTPUT_DIR
+srun --mpi=pmi2 python PROX_sdf_calculation_following_prox_MPI.py --scans_dir $PROX_SCANS_DIR  --scene MPH1Library --grid_dim $GRID_DIM --output_dir $PROX_OUTPUT_DIR
 echo "working on MPH8"
-srun --mpi=pmi2 python 02_sdf_calculation_following_prox_MPI.py --scans_dir $PROX_SCANS_DIR  --scene MPH8 --grid_dim $GRID_DIM --output_dir $PROX_OUTPUT_DIR
+srun --mpi=pmi2 python PROX_sdf_calculation_following_prox_MPI.py --scans_dir $PROX_SCANS_DIR  --scene MPH8 --grid_dim $GRID_DIM --output_dir $PROX_OUTPUT_DIR
 echo "working on N0SittingBooth"
-srun --mpi=pmi2 python 02_sdf_calculation_following_prox_MPI.py --scans_dir $PROX_SCANS_DIR  --scene N0SittingBooth --grid_dim $GRID_DIM --output_dir $PROX_OUTPUT_DIR
+srun --mpi=pmi2 python PROX_sdf_calculation_following_prox_MPI.py --scans_dir $PROX_SCANS_DIR  --scene N0SittingBooth --grid_dim $GRID_DIM --output_dir $PROX_OUTPUT_DIR
 echo "working on N0Sofa"
-srun --mpi=pmi2 python 02_sdf_calculation_following_prox_MPI.py --scans_dir $PROX_SCANS_DIR  --scene N0Sofa --grid_dim $GRID_DIM --output_dir $PROX_OUTPUT_DIR
+srun --mpi=pmi2 python PROX_sdf_calculation_following_prox_MPI.py --scans_dir $PROX_SCANS_DIR  --scene N0Sofa --grid_dim $GRID_DIM --output_dir $PROX_OUTPUT_DIR
 echo "working on N3Library"
-srun --mpi=pmi2 python 02_sdf_calculation_following_prox_MPI.py --scans_dir $PROX_SCANS_DIR  --scene N3Library --grid_dim $GRID_DIM --output_dir $PROX_OUTPUT_DIR
+srun --mpi=pmi2 python PROX_sdf_calculation_following_prox_MPI.py --scans_dir $PROX_SCANS_DIR  --scene N3Library --grid_dim $GRID_DIM --output_dir $PROX_OUTPUT_DIR
 echo "working on N3Office"
-srun --mpi=pmi2 python 02_sdf_calculation_following_prox_MPI.py --scans_dir $PROX_SCANS_DIR  --scene N3Office --grid_dim $GRID_DIM --output_dir $PROX_OUTPUT_DIR
+srun --mpi=pmi2 python PROX_sdf_calculation_following_prox_MPI.py --scans_dir $PROX_SCANS_DIR  --scene N3Office --grid_dim $GRID_DIM --output_dir $PROX_OUTPUT_DIR
 echo "working on N3OpenArea"
-srun --mpi=pmi2 python 02_sdf_calculation_following_prox_MPI.py --scans_dir $PROX_SCANS_DIR  --scene N3OpenArea --grid_dim $GRID_DIM --output_dir $PROX_OUTPUT_DIR
+srun --mpi=pmi2 python PROX_sdf_calculation_following_prox_MPI.py --scans_dir $PROX_SCANS_DIR  --scene N3OpenArea --grid_dim $GRID_DIM --output_dir $PROX_OUTPUT_DIR
 echo "working on Werkraum"
-srun --mpi=pmi2 python 02_sdf_calculation_following_prox_MPI.py --scans_dir $PROX_SCANS_DIR  --scene Werkraum --grid_dim $GRID_DIM --output_dir $PROX_OUTPUT_DIR
+srun --mpi=pmi2 python PROX_sdf_calculation_following_prox_MPI.py --scans_dir $PROX_SCANS_DIR  --scene Werkraum --grid_dim $GRID_DIM --output_dir $PROX_OUTPUT_DIR
